da73f9de014a87b67c9fb9ee1a09db1f6ce77d87
liggidarck <liggidarck@gmail.com>
2025-03-02 21:47:52 +0300

    fix lombok

--- src/main/java/com/employe/employemanagementapp/controller/EmployerController.java
+++ src/main/java/com/employe/employemanagementapp/controller/EmployerController.java
@@ -2,6 +2,9 @@ package com.employe.employemanagementapp.controller;
 
 import com.employe.employemanagementapp.model.Employer;
 import com.employe.employemanagementapp.service.EmployerService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/employees")
 public class EmployerController {
+    private final Logger logger = LoggerFactory.getLogger(EmployerController.class);
 
     private final EmployerService employerService;
 
@@ -23,6 +27,7 @@ public class EmployerController {
     // Обработка POST запроса на добавление
     @PostMapping
     public ResponseEntity<Employer> addEmployer(@RequestBody Employer employer) {
+        logger.warn("employer : {}", employer.getFirstName());
         Employer savedEmployer = employerService.addEmployer(employer);
         return ResponseEntity.ok(savedEmployer);
     }
